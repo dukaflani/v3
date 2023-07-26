cc2dfb37e7f885a6d061dcfa40e05c02ff58ba23
Jidraff Gathura <jidraff.social@gmail.com>
2023-07-26 13:47:11 +0300

    minor edits on getsponsered getter fn

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -155,7 +155,7 @@ export const getFeaturedEvents = async () => {
 }
 
 export const getSponsoredEvents = async () => {
-    const response = await api.get(`/store/events/?is_sponsored=true`)
+    const response = await api.get(`/store/events/?is_featured=true`)
     return response.data
 }
 
