312c1efd9a7661aa4ae6c288dc5f863379ba2270
Jidraff Gathura <jidraff.social@gmail.com>
2023-08-28 17:45:51 +0300

    changed seo tags from dynamic to static on profile pages

--- src/pages/_viewport/desktop/[username].js
+++ src/pages/_viewport/desktop/[username].js
@@ -43,14 +43,14 @@ const UserProfile = ({ setIsDarkMode, isDarkMode, }) => {
         <NavigationLayout2 setIsDarkMode={setIsDarkMode} isDarkMode={isDarkMode} >
             <Head>
                     <title>{profileUsername ? `@${profileUsername} - Dukaflani Profile` : 'Loading profile...'}</title>
-                    <meta name="title" content={`${profileUsername} profile on Dukaflani`} />
+                    <meta name="title" content={`My profile on Dukaflani`} />
                     <meta name="description" content="A dynamic link-in-bio solution built for the modern African Artist with support for streaming links, merchandise, lyrics, skiza tunes, albums, events and media tours"/>
                     <meta name="keywords" content="Music Videos, Dukaflani, Links, Events, Merchandise, Skiza Tune, Lyrics, Albums, Celebrity Merchandise, Name Brands"/>
 
                     
                     <meta property="og:type" content="website"/>
                     <meta property="og:url" content={`${process.env.NEXT_PUBLIC_NEXT_URL}/${profileUsername}`} />
-                    <meta property="og:title" content={`${profileUsername} profile on Dukaflani`} />
+                    <meta property="og:title" content={`My profile on Dukaflani`} />
                     <meta property="og:description" content="A dynamic link-in-bio solution built for the modern African Artist with support for streaming links, merchandise, lyrics, skiza tunes, albums, events and media tours"/>
                     <meta 
                         property="og:image" 
